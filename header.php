8a5183243b473e6e3da7bee6d80452ea563922eb
Angelo Maramara <angelo.maramara@gmail.com>
2016-07-04 15:20:23 -0400

    Add logo and position

--- header.php
+++ header.php
@@ -12,28 +12,30 @@
     <header class="row">
       <!-- Begin Sidebar -->
       <div class="three columns leftsidenav">
-        <!-- Navigation -->
-        <div class="nine columns">
-          <?php
-              wp_nav_menu(array(
-                'sort_column' => 'menu_order',
-                'container_class' => 'blank-menu-header'
-              ));
-          ?>
-        </div>
+          <div id="logo">
+              am
+          </div>
+          <!-- Navigation -->
+          <div class="nine columns">
+            <?php
+                wp_nav_menu(array(
+                  'sort_column' => 'menu_order',
+                  'container_class' => 'blank-menu-header'
+                ));
+            ?>
+          </div>
           <?php dynamic_sidebar('front-sidebar-widget'); ?>
       </div>
       <!-- End Sidebar -->
 
       <!-- Header Picture and Title -->
-      <div class="eight columns headbgtitle">
-        <h1 class="site-title"><a href="<?php $url = home_url('/'); echo $url;?>">
-          <?php bloginfo('name'); ?>
-        </a></h1>
-        <h2 class="sub-title"><?php bloginfo('description'); ?></h2>
-      </div>
-      <div class="four columns">
-        <h3>Search Bar</h3>
+      <div class="nine columns headbgtitle">
+          <div>
+            <h1 class="site-title"><a href="<?php $url = home_url('/'); echo $url;?>">
+              <?php bloginfo('name'); ?>
+            </a></h1>
+            <h2 class="sub-title"><?php bloginfo('description'); ?></h2>
+          </div>
       </div>
     </header>
     <div class="row">
