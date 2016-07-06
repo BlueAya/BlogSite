b6a5180a244323b9e0d25e63d004391a62438d2d
Angelo Maramara <angelo.maramara@gmail.com>
2016-07-06 01:23:23 -0400

    Setup Archives page

--- archive.php
+++ archive.php
@@ -3,8 +3,8 @@
 
 get_header(); ?>
 
-<div class="row">
-    <div class="twelve columns">
+<div class="main-content-archives" class="nine columns">
+    <div class="nine columns main-content-archives">
         <h2>Archives</h2>
         <?php if ( have_posts() ) :
             // The Loop
@@ -17,9 +17,7 @@ get_header(); ?>
             <p>Sorry, no posts matched your criteria.</p>
         <?php endif; ?>
     </div>
-</div>
-<div class="row">
-    <div class="twelve columns">
+    <div class="nine columns main-content-archives">
         <h2>Archives by Month:</h2>
         <ul>
             <?php wp_get_archives('type=monthly'); ?>
@@ -29,5 +27,7 @@ get_header(); ?>
              <?php wp_list_categories(); ?>
         </ul>
     </div>
+
 </div>
+
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -28,15 +28,16 @@
                     the_post(); ?>
                 <div class="row">
                     <div class="three columns index-thumbnail">
+
                     <?php if ( has_post_thumbnail() ) { ?>
-                              <?php the_post_thumbnail('thumbnail'); ?>
+                              <?php the_post_thumbnail(array(400,400)); ?>
                           <?php }
                     ?>
                   </div>
                       <div class="nine columns">
-                          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                          <?php the_excerpt(); ?>
-                          <a href="<?php the_permalink(); ?>">Read More</a>
+                          <h3><a class="titlenearpermalinks" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                          <!-- <?php the_excerpt(); ?> -->
+                          <a class="permalinks" href="<?php the_permalink(); ?>">Read More</a>
                       </div>
                   </div>
 
@@ -55,14 +56,13 @@
                 <div class="row">
                     <div class="three columns index-thumbnail">
                     <?php if ( has_post_thumbnail() ) { ?>
-                              <?php the_post_thumbnail('thumbnail'); ?>
+                              <?php the_post_thumbnail(array(350,350)); ?>
                           <?php }
                     ?>
                   </div>
                       <div class="nine columns">
-                          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                          <?php the_excerpt(); ?>
-                          <a href="<?php the_permalink(); ?>">Read More</a>
+                          <h3><a class="titlenearpermalinks2" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                          <a class="permalinks2" href="<?php the_permalink(); ?>">Read More</a>
                       </div>
                   </div>
 
