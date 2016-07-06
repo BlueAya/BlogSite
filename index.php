90aee1317ce90f6006baecfc4ab3b638612f0c9b
Angelo Maramara <angelo.maramara@gmail.com>
2016-07-05 23:14:08 -0400

    Add two column layout

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!-- End Header -->
 
     <!-- Begin Section Container -->
-    <section class="row">
+    <section id="main-content" class="row nine columns">
 
       <!-- Sidebar -->
       <!-- <div class="four columns leftsidenav"> -->
@@ -20,8 +20,35 @@
       <!-- </div> -->
 
       <!-- End Sidebar -->
-      <div class="eight columns main-content">
+      <div class="seven columns main-content">
           <!-- Begin Loop -->
+          <?php query_posts('cat=1&showposts=3'); ?>
+              <?php if ( have_posts() ) {
+                  while ( have_posts() ) {
+                    the_post(); ?>
+                <div class="row">
+                    <div class="three columns index-thumbnail">
+                    <?php if ( has_post_thumbnail() ) { ?>
+                              <?php the_post_thumbnail('thumbnail'); ?>
+                          <?php }
+                    ?>
+                  </div>
+                      <div class="nine columns">
+                          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                          <?php the_excerpt(); ?>
+                          <a href="<?php the_permalink(); ?>">Read More</a>
+                      </div>
+                  </div>
+
+                    <?php
+                    } //end while
+                } //end if
+              ?>
+              <!-- End Loop -->
+      </div>
+      <div class="five columns main-content-right">
+          <!-- Begin Loop -->
+          <?php query_posts('cat=-1&showposts=3'); ?>
               <?php if ( have_posts() ) {
                   while ( have_posts() ) {
                     the_post(); ?>
